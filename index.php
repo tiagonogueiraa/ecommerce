3c11906b9135fe5ae03cce614039b0e8f575ae29
Tiago Nogueira <tiagonogueirajf@gmail.com>
2019-01-17 23:05:04 -0200

    Primeira classe do template

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php 
-
+//traz o que o projeto precisa
 require_once("vendor/autoload.php");
-
-$app = new \Slim\Slim();
+//qual classe eu quero
+use \Slim\Slim;
+use \Hcode\Page;
+//nova rota
+$app = new Slim();
 
 $app->config('debug', true);
-
+//chamar meu site sem rota venha para aqui e carrega a paginas
 $app->get('/', function() {
+
+	$page = new Page();
+
+	$page->setTpl("index");
+
+
     
-	$sql = new Hcode\DB\Sql();
+	//$sql = new Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	//$results = $sql->select("SELECT * FROM tb_users");
 
-	echo json_encode($results);
+//	echo json_encode($results);
 
 });
 
---
+++ vendor/hcodebr/php-classes/src/DB/Page.php
@@ -0,0 +1,83 @@
+<?php 
+
+
+namespace Hcode;
+
+use Rain\Tpl;
+
+class Page {
+
+	//declarar
+	private $tpl;
+	private $options = [];
+	private $defaults = [
+		"data"=>[]
+
+	];
+
+	public function __construct($opts = array()){
+
+ 		// um sobreescreve o outro, e o construct sobressai o defaults e vale o opts
+	$this->options = array_merge($this->defaults, $opts);
+
+		// config
+	$config = array(
+					"tpl_dir"       => $_SERVER['DOCUMENT_ROOT']."/views/",
+					"cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
+					"debug"         => false // set to false to improve the speed
+					//NAO PRECISA DESSA PARTE, ou coloque false. | "debug"         => true // set to false to improve the speed
+				   );
+
+	Tpl::configure($config);
+
+	//criar o tpl
+
+	//otimizado
+	$this->setData($this->option["data"]);
+
+	//$this->tpl =>tpl = new Tpl;
+
+	//foreach ($this->options["data"] as $key => $value) {
+		# code...
+		//recebe os arrays que sao as variaveis options ou defaults
+	//	$this->tpl->assign($key,$value);
+
+	//}
+
+	//desenhar o template na tela
+	//draw eh uma funcao do tpl
+	$this->tpl->draw("header");
+
+
+	}
+
+	public function setTpl($name, $data = array(), $returnHTML = false)
+	{
+		
+		$this->setData($data);
+
+		return $this->tpl->draw($name, $returnHTML);
+
+	}
+
+}
+
+	private function setData($data = array())
+	{
+		foreach ($data as $key => $value) {
+		# code...
+		//recebe os arrays que sao as variaveis options ou defaults
+		$this->tpl->assign($key,$value);
+
+	}
+	}
+	
+
+	public function __destruct(){
+
+		$this->tpl->draw("footer");
+
+	}
+}
+
+ ?>
